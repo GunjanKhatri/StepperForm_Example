dee08a18aa18bb0d48946ca4a43410840aa682d6
Gunjan Advani <Gunjan.Advani@shell.com>
2022-08-27 15:20:21 +0530

    Add project files.

---
+++ Institute_MVC/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Institute_MVC.Core;
+using Institute_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Institute_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Account()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Account(Account account)
+        {
+            var myData = new BasicDetails();
+            myData.accountDetails = new Account();
+            myData.accountDetails = account;
+
+            TempData["basicDetails"] = myData;
+            return RedirectToAction("Payment");
+        }
+
+        public ActionResult Personal()
+        {
+           return View();
+        }
+        [HttpPost]
+        public ActionResult Personal(Personal personal)
+        {
+            var myData = new BasicDetails();
+            myData = (BasicDetails)TempData["basicDetails"];
+
+            myData.personalDetails = new Personal();
+            myData.personalDetails = personal;
+
+            //TempData["basicDetails"] = myData;
+            return RedirectToAction("Payment");
+        }
+
+        public ActionResult Payment()
+        {
+            
+
+            return View();
+        }
+        public ActionResult Finish()
+        {
+            
+
+            return View();
+        }
+    }
+}
---
+++ Institute_MVC/Models/BasicDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Institute_MVC.Models
+{
+    public class BasicDetails
+    {
+        public Account accountDetails { get; set; }
+        public Personal personalDetails { get; set; }
+    }
+}
